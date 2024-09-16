895e963379e4d44c8f24426fcbf81659c8fec3ac
arojas <arojas@bsc.com.do>
2024-09-16 15:55:42 -0400

    Subiendo a Develo, agregando id's

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
+
 /* Control para el Switch del DarkMode and LightMode */
 document.addEventListener("DOMContentLoaded", (event) => {
   const htmlElement = document.documentElement;
   const switchElement = document.getElementById("darkModeSwitch");
 
-  // Set the default theme to dark if no setting is found in local storage
+  // Por default el color de la pagina comenzara en dark el cual se almacena en LocalStorage
   const currentTheme = localStorage.getItem("bsTheme") || "dark";
   htmlElement.setAttribute("data-bs-theme", currentTheme);
   switchElement.checked = currentTheme === "dark";
@@ -19,12 +20,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
-/* JavaScript for Countdown */
+/* JavaScript Cuenta Regresiva */
 
-// Time in seconds before redirect
+// Tiempo que se hara el Redirect
 const redirectTime = 100;
 
-// Update the countdown and set a timeout for redirect
 function updateCountdown() {
   const countdownElement = document.getElementById("countdown");
   let timeLeft = redirectTime;
@@ -35,6 +35,7 @@ function updateCountdown() {
 
     if (timeLeft <= 1) {
       clearInterval(countdownInterval);
+      // Aqui colocamos a cual documento html haria el redirect.
       window.location.href = "../html/TodoList.html";
     }
   }, 1000);
